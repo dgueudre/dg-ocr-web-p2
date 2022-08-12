388b2892262515cecc479ab2bcdc9d2bf9ca8ab7
Davy Gueudré <davy.gueudre@prima-solutions.group>
2022-08-12 17:05:33 +0200

    fixes

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ module.exports = config => {
   config.addPassthroughCopy('src/css');
 
 
-  config.addNunjucksFilter("filter_by", (links, propertyName, value) => links.filter(link => link[propertyName] === value));
+  config.addNunjucksFilter("filter_by", (links, property, value) => links.filter(link => link[property] === value));
 
   return {
     markdownTemplateEngine: 'njk',
